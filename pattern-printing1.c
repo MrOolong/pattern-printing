fdb94fe13ff6e2d3ee0bd383aeddb088893843ce
Dennis Daubney Jr <dennis.daubney@gmail.com>
2017-10-30 01:36:43 +0000

    fixed errors

--- pattern-printing1.c
+++ pattern-printing1.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
-void main () {
-    
+int main () {
+
     char print='*';
     int row,col;
     int noOfRows=5;
-    
+
     for(row = 1; row <= noOfRows; row++) {
-        
-        for(col = 1; col <= rows; col++) {
-            
+
+        for(col = 1; col <= row; col++) {
+
         printf("%c", print);
-        
+
         }
         printf("\n");
     }
-    
+
 }
