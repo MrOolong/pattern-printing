d6f18d3c04aeb73358b5cabada95d3b8d31188b2
Dennis Daubney Jr <dennis.daubney@gmail.com>
2017-10-30 01:25:27 +0000

    pattern-printing 1 lesson

--- pattern-printing1.c
+++ pattern-printing1.c
@@ -1,1 +1,19 @@
 #include <stdio.h>
+
+void main () {
+    
+    char print='*';
+    int row,col;
+    int noOfRows=5;
+    
+    for(row = 1; row <= noOfRows; row++) {
+        
+        for(col = 1; col <= rows; col++) {
+            
+        printf("%c", print);
+        
+        }
+        printf("\n");
+    }
+    
+}
